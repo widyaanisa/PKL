860cd7a9e31f0b5a92c7a21edb454ac8956eaa8c
Nur Widya Anisa Muslim <74421737+widyaanisa@users.noreply.github.com>
2021-10-28 00:13:08 +0800

    update 28/10

--- js/sistem.js
+++ js/sistem.js
@@ -54,4 +54,30 @@
     });
   
   })(jQuery); // End of use strict
+
+  //js untuk see/hide password
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
   
+  //js untuk mencocokaan new & confirm password
+  var newPassword = document.getElementById("new_password");
+  var confirmPassword = document.getElementById("confirm_password");
+
+  function validatePassword() {
+      if (newPassword.value != confirmPassword.value) {
+          document.getElementById("confirm_password").setCustomValidity("Passwords do not match!");
+      } else {
+          //empty string means no validation error
+          document.getElementById("confirm_password").setCustomValidity('');
+      }
+  }
+  newPassword.addEventListener("change", validatePassword);
+  confirmPassword.addEventListener("change", validatePassword);
